0bcd3664df849a25076597690a72230d0f6b1ed6
Ross Burton <ross@burtonini.com>
2008-12-02 17:55:13 +0000

    Pull out the feed parsing and handle more status codes

--- feednotify.py
+++ feednotify.py
@@ -25,6 +25,8 @@ pynotify.init("FeedNotify")
 
 feedparser.USER_AGENT = "FeedNotify/%s +http://burtonini.com/" % VERSION
 
+feeds = []
+
 class Feed:
     def __init__(self, title, url):
         self.title = title
@@ -35,30 +37,39 @@ class Feed:
         self.etag = None
         self.modified = None
 
+    def parse(self, feed):
+        self.etag = feed.etag
+        self.modified = feed.modified
+        count = 0
+
+        for entry in feed.entries:
+            if entry.id not in self.seen:
+                self.seen.add(entry.id)
+                    # TODO: improve this to be the N most recent unseen posts
+                if count < 5:
+                    count = count + 1
+                    message = "<a href='%s'>%s</a>" % (entry.link, entry.title)
+                    n = pynotify.Notification(self.title, message)
+                    n.set_category("email.arrived")
+                    n.set_urgency(0)
+                    n.show()
+
     def run(self):
         feed = feedparser.parse(self.url, etag=self.etag, modified=self.modified)
         # TODO: handle errors and popup a error notification
         if feed.status == 200:
-            self.etag = feed.etag
-            self.modified = feed.modified
-            count = 0
+            # OK
+            self.parse(feed)
+        elif feed.status == 301:
+            # Moved Permanently
+            self.url = feed.url
+            self.parse(feed)
+        elif feed.status == 401:
+            # Gone
+            del feeds[feed]
 
-            for entry in feed.entries:
-                if entry.id not in self.seen:
-                    self.seen.add(entry.id)
-                    # TODO: improve this to be the N most recent unseen posts
-                    if count < 5:
-                        count = count + 1
-                        message = "<a href='%s'>%s</a>" % (entry.link, entry.title)
-                        n = pynotify.Notification(self.title, message)
-                        n.set_category("email.arrived")
-                        n.set_urgency(0)
-                        n.show()
-
-feeds = (
-    Feed("Guardian", "http://feeds.guardian.co.uk/theguardian/rss"),
-    Feed("Comment Is Free", "http://feeds.guardian.co.uk/theguardian/commentisfree/rss"),
-)
+feeds.append(Feed("Guardian", "http://feeds.guardian.co.uk/theguardian/rss"))
+feeds.append(Feed("Comment Is Free", "http://feeds.guardian.co.uk/theguardian/commentisfree/rss"))
 
 if __name__ == "__main__":
     import time
