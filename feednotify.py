718b8d8c349a348445297975efecd113f1ab6a07
Ross Burton <ross@burtonini.com>
2008-12-02 17:26:39 +0000

    Refactor a Feed object and add Comment Is Free

--- feednotify.py
+++ feednotify.py
@@ -2,34 +2,43 @@
 
 import feedparser, time, pynotify
 
-# TODO: add a Feed abstraction with a title and URL
-
 # TODO: somehow add a "save for later" button which does... something.
-
-# TODO: copy the seen set each run and then remove IDs we get so that old IDs
-# can be removed from the persistant set
-seen = set()
-etag = None
-modified = None
 
 pynotify.init("FeedNotify")
 
-while True:
-    feed = feedparser.parse("http://feeds.guardian.co.uk/theguardian/rss", etag=etag, modified=modified)
-    if feed.status == 200:
-        etag = feed.etag
-        modified = feed.modified
-        count = 0
+class Feed:
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+        # TODO: copy the seen set each run and then remove IDs we get so that old IDs
+        # can be removed from the persistant set
+        self.seen = set()
+        self.etag = None
+        self.modified = None
 
-        for entry in feed.entries:
-            if entry.id not in seen:
-                seen.add(entry.id)
-                # TODO: improve this to be the N most recent unseen posts
-                if count < 5:
-                    count = count + 1
-                    message = "<a href='%s'>%s</a>" % (entry.link, entry.title)
-                    n = pynotify.Notification("Latest News", message)
-                    n.set_category("email.arrived")
-                    n.set_urgency(0)
-                    n.show()
-    time.sleep(60)
+    def run(self):
+        feed = feedparser.parse(self.url, etag=self.etag, modified=self.modified)
+        if feed.status == 200:
+            self.etag = feed.etag
+            self.modified = feed.modified
+            count = 0
+
+            for entry in feed.entries:
+                if entry.id not in self.seen:
+                    self.seen.add(entry.id)
+                    # TODO: improve this to be the N most recent unseen posts
+                    if count < 5:
+                        count = count + 1
+                        message = "<a href='%s'>%s</a>" % (entry.link, entry.title)
+                        n = pynotify.Notification(self.title, message)
+                        n.set_category("email.arrived")
+                        n.set_urgency(0)
+                        n.show()
+
+feeds = (
+    Feed("Guardian", "http://feeds.guardian.co.uk/theguardian/rss"),
+    Feed("Comment Is Free", "http://feeds.guardian.co.uk/theguardian/commentisfree/rss"),
+)
+
+while True:
+    [feed.run() for feed in feeds]
